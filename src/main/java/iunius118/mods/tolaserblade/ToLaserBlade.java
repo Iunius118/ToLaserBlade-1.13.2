e2e6fb974d09bd32c31c0bf5de080dfa93085e3a
Iunius118 <arcd.01234567@gmail.com>
2016-06-04 15:45:20 +0900

    Require Forge [1.9.4-12.17.0.1948,)

--- src/main/java/iunius118/mods/tolaserblade/ToLaserBlade.java
+++ src/main/java/iunius118/mods/tolaserblade/ToLaserBlade.java
@@ -28,7 +28,7 @@ public class ToLaserBlade {
 	public static final String MOD_ID = "tolaserblade";
 	public static final String MOD_NAME = "ToLaserBlade";
 	public static final String MOD_VERSION = "0.0.3[WIP]";
-	public static final String MOD_DEPENDENCIES = "required-after:Forge@[1.9.4-12.17.0.1940,)";
+	public static final String MOD_DEPENDENCIES = "required-after:Forge@[1.9.4-12.17.0.1948,)";
 	public static final String MOD_ACCEPTED_MC_VERSIONS = "[1.9.4]";
 
 	@EventHandler
