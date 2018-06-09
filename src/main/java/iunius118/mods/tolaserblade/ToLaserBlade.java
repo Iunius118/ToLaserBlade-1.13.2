dbcadb6a893f635e5b1ba394dc95f9b1563f8281
Iunius118 <arcd.01234567@gmail.com>
2018-06-10 01:01:03 +0900

    Add update notification

--- src/main/java/iunius118/mods/tolaserblade/ToLaserBlade.java
+++ src/main/java/iunius118/mods/tolaserblade/ToLaserBlade.java
@@ -15,15 +15,24 @@ import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.client.model.obj.OBJLoader;
+import net.minecraftforge.common.ForgeVersion;
+import net.minecraftforge.common.ForgeVersion.CheckResult;
+import net.minecraftforge.common.ForgeVersion.Status;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -31,11 +40,18 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(modid = ToLaserBlade.MOD_ID, name = ToLaserBlade.MOD_NAME, version = ToLaserBlade.MOD_VERSION, dependencies = ToLaserBlade.MOD_DEPENDENCIES, useMetadata = true)
+@Mod(
+        modid = ToLaserBlade.MOD_ID,
+        name = ToLaserBlade.MOD_NAME,
+        version = ToLaserBlade.MOD_VERSION,
+        dependencies = ToLaserBlade.MOD_DEPENDENCIES,
+        updateJSON = ToLaserBlade.MOD_UPDATE_JSON_URL,
+        useMetadata = true)
 @EventBusSubscriber
 public class ToLaserBlade
 {
@@ -44,6 +60,7 @@ public class ToLaserBlade
     public static final String MOD_NAME = "ToLaserBlade";
     public static final String MOD_VERSION = "${version}";
     public static final String MOD_DEPENDENCIES = "required-after:forge@[1.12.2-14.23.3.2655,)";
+    public static final String MOD_UPDATE_JSON_URL = "https://raw.githubusercontent.com/Iunius118/ToLaserBlade/master/update.json";
 
     public static final ToolMaterial MATERIAL_LASAR = EnumHelper.addToolMaterial("LASAR", 3, 255, 12.0F, 1.0F, 22).setRepairItem(new ItemStack(net.minecraft.init.Blocks.REDSTONE_TORCH));
     public static final ToolMaterial MATERIAL_LASER = EnumHelper.addToolMaterial("LASER", 3, 32767, 12.0F, 3.0F, 22).setRepairItem(new ItemStack(net.minecraft.init.Items.REDSTONE));
@@ -54,6 +71,8 @@ public class ToLaserBlade
     public static final String NAME_ITEM_LASER_BLADE = "laser_blade";
     public static final ModelResourceLocation MRL_ITEM_LASER_BLADE = new ModelResourceLocation(MOD_ID + ":" + NAME_ITEM_LASER_BLADE, "inventory");
     public static final ResourceLocation RL_OBJ_ITEM_LASER_BLADE = new ResourceLocation(MOD_ID, "item/laser_blade.obj");
+
+    public static boolean hasShownUpdate = false;
 
     @SidedProxy
     public static CommonProxy proxy;
@@ -186,6 +205,41 @@ public class ToLaserBlade
             }
 
             return null;
+        }
+
+
+        @SubscribeEvent
+        public void onConnectedToServer(ClientConnectedToServerEvent event)
+        {
+            if (!hasShownUpdate)
+            {
+                // Check update and Notify client
+                CheckResult result = ForgeVersion.getResult(Loader.instance().activeModContainer());
+                Status status = result.status;
+
+                if (status == Status.PENDING)
+                {
+                    // Failed to get update information
+                    return;
+                }
+
+                if (status == Status.OUTDATED || status == Status.BETA_OUTDATED)
+                {
+                    ITextComponent modNameHighlighted = new TextComponentString(MOD_NAME);
+                    modNameHighlighted.getStyle().setColor(TextFormatting.YELLOW);
+
+                    ITextComponent newVersionHighlighted = new TextComponentString(result.target.toString());
+                    newVersionHighlighted.getStyle().setColor(TextFormatting.YELLOW);
+
+                    ITextComponent message = new TextComponentTranslation("tolaserblade.message.update", modNameHighlighted).appendText(": ").appendSibling(newVersionHighlighted);
+                    message.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, result.url));
+
+                    Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(message);
+
+                    hasShownUpdate = true;
+                }
+
+            }
         }
 
     }
