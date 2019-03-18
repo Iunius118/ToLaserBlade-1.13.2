6a69cc1d17cb2ff521740dfa6660de84aa81a835
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2019-03-18 20:24:23 +0900

    Now broken Laser Blades give Laser Blade Cores

--- src/main/java/com/github/iunius118/tolaserblade/item/ItemEventHandler.java
+++ src/main/java/com/github/iunius118/tolaserblade/item/ItemEventHandler.java
@@ -1,15 +1,28 @@
 package com.github.iunius118.tolaserblade.item;
 
 import com.github.iunius118.tolaserblade.ToLaserBlade;
+import com.github.iunius118.tolaserblade.item.ItemLaserBlade.LaserBlade;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.event.entity.player.AnvilRepairEvent;
 import net.minecraftforge.event.entity.player.CriticalHitEvent;
+import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemCraftedEvent;
 
 public class ItemEventHandler {
+	@SubscribeEvent
+	public void onPlayerDestroyItem(PlayerDestroyItemEvent event) {
+		ItemStack original = event.getOriginal();
+
+		if (original.getItem() == ToLaserBlade.Items.laser_blade) {
+			LaserBlade laserBlade = new LaserBlade(original);
+			ItemStack core = laserBlade.saveTagsToItemStack(ToLaserBlade.Items.laser_blade_core.getDefaultInstance());
+			event.getEntityPlayer().setHeldItem(event.getHand(), core);
+		}
+	}
+
 	@SubscribeEvent
 	public void onCriticalHit(CriticalHitEvent event) {
 
